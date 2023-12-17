48f43242749f9e4cacf17a0b4eb627df4883c8b8
Michael-128 <michal290704@gmail.com>
2023-12-17 01:30:52 +0100

    Added NAS Icon

--- Components/Cards/SystemCard.tsx
+++ Components/Cards/SystemCard.tsx
@@ -1,18 +1,28 @@
-import { Card, Icon, Text } from "@ui-kitten/components"
+import { Card, Icon, Text, useTheme } from "@ui-kitten/components"
 import { CIconHeader } from "../Typography/CIconHeader"
 import { CLabel } from "../Typography/CLabel"
+import { View } from "react-native"
 
 export default function SystemCard() {
+    const theme = useTheme()
+
     return (
         <Card>
             <CIconHeader iconName="nas">
                 System
             </CIconHeader>
 
-            <CLabel name="Platform" value="Generic" />
-            <CLabel name="Version" value="Scale" />
-            <CLabel name="Hostname" value="truenas" />
-            <CLabel name="Uptime" value="1 day" />
+            <View style={{flexDirection: "row", justifyContent: "space-between"}}>
+                <View style={{justifyContent: "space-evenly"}}>
+                    <CLabel name="Platform" value="Generic" />
+                    <CLabel name="Version" value="Scale" />
+                    <CLabel name="Hostname" value="truenas" />
+                    <CLabel name="Uptime" value="1 day" />
+                </View>
+
+                <Icon style={{height: 100, width: 100, color: theme["color-primary-500"]}} name="nas" pack="material"/>
+            </View>
+            
         </Card>
     )
 }
