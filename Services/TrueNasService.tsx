edcf55176c54483e158d744747318422bd26bf1f
Michael-128 <michal290704@gmail.com>
2025-02-06 18:50:34 +0100

    Delete unnecessary code & files

--- Services/TrueNasService.tsx
+++ Services/TrueNasService.tsx
@@ -113,6 +113,15 @@ class TrueNasService {
             })
         }
     }
+
+    // export async function postScaleReplicas(url: string, token: string, releaseName: string, replicaCount: number) {
+    //     const res = await postFetch(`${url}/api/v2.0/chart/release/scale`, token, {
+    //         release_name: releaseName,
+    //         scale_options: {
+    //             replica_count: replicaCount
+    //         }
+    //     })
+    // }
 }
 
 export default TrueNasService;
